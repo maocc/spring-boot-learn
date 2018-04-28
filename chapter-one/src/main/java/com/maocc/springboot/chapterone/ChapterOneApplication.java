d3829d7c14e1d5889c649cadbd62ac769d5794ae
maocc <maochong2009@126.com>
2018-04-28 18:02:42 +0800

    Add files via upload

---
+++ chapter-one/src/main/java/com/maocc/springboot/chapterone/ChapterOneApplication.java
@@ -0,0 +1,12 @@
+package com.maocc.springboot.chapterone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapterOneApplication.class, args);
+	}
+}
